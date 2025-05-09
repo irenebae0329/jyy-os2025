ae1b46e84fdfa008ae60cf2d44e6c786893e0c19
tracer-nju <tracer@nju.edu.cn>
2025-05-09 16:40:45 +0800

    Darwin W22WY4FD6V 24.3.0 Darwin Kernel Version 24.3.0: Thu Jan  2 20:24:16 PST 2025; root:xnu-11215.81.4~3/RELEASE_ARM64_T6000 arm64 16:40  up 16 days, 19:02, 1 user, load averages: 6.02 4.38 4.21

--- .shadow/labyrinth/util.c
+++ .shadow/labyrinth/util.c
@@ -5,14 +5,14 @@
 #include <stdbool.h>
 void joinPath(char *dst, size_t size, const char *dir, const char *filenames[]) {
     if (dir == NULL || filenames == NULL || dst == NULL) return;
-    size_t len = strlen(dir);
-    for(int i = 0; i < sizeof(filenames); i++) {
-        char *filename = filenames[i];
-        if (len > 0 && dir[len-1] == '/')
-            snprintf(dst, size, "%s%s", dir, filename);
-        else
-            snprintf(dst, size, "%s/%s", dir, filename);
-    }
+    // size_t len = strlen(dir);
+    // for(int i = 0; i < sizeof(filenames); i++) {
+    //     char *filename = filenames[i];
+    //     if (len > 0 && dir[len-1] == '/')
+    //         snprintf(dst, size, "%s%s", dir, filename);
+    //     else
+    //         snprintf(dst, size, "%s/%s", dir, filename);
+    // }
 }
 
 bool file_exists(const char *filename) {
