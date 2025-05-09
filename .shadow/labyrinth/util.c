790869b9d879c1807b18edf44bb2f6e1da894bc6
tracer-nju <tracer@nju.edu.cn>
2025-05-09 16:41:20 +0800

    Darwin W22WY4FD6V 24.3.0 Darwin Kernel Version 24.3.0: Thu Jan  2 20:24:16 PST 2025; root:xnu-11215.81.4~3/RELEASE_ARM64_T6000 arm64 16:41  up 16 days, 19:03, 1 user, load averages: 6.84 4.77 4.35

--- .shadow/labyrinth/util.c
+++ .shadow/labyrinth/util.c
@@ -6,13 +6,13 @@
 void joinPath(char *dst, size_t size, const char *dir, const char *filenames[]) {
     if (dir == NULL || filenames == NULL || dst == NULL) return;
     size_t len = strlen(dir);
-    // for(int i = 0; i < sizeof(filenames); i++) {
-    //     char *filename = filenames[i];
-    //     if (len > 0 && dir[len-1] == '/')
-    //         snprintf(dst, size, "%s%s", dir, filename);
-    //     else
-    //         snprintf(dst, size, "%s/%s", dir, filename);
-    // }
+    for(int i = 0; i < sizeof(filenames); i++) {
+        // char *filename = filenames[i];
+        // if (len > 0 && dir[len-1] == '/')
+        //     snprintf(dst, size, "%s%s", dir, filename);
+        // else
+        //     snprintf(dst, size, "%s/%s", dir, filename);
+    }
 }
 
 bool file_exists(const char *filename) {
