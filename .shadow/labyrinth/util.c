ecc3189e92e365bff31ff73f070e59e2bc22c64d
tracer-nju <tracer@nju.edu.cn>
2025-05-09 16:47:40 +0800

    Darwin W22WY4FD6V 24.3.0 Darwin Kernel Version 24.3.0: Thu Jan  2 20:24:16 PST 2025; root:xnu-11215.81.4~3/RELEASE_ARM64_T6000 arm64 16:47  up 16 days, 19:09, 1 user, load averages: 1.85 3.15 3.74

--- .shadow/labyrinth/util.c
+++ .shadow/labyrinth/util.c
@@ -6,13 +6,12 @@
 void joinPath(char *dst, size_t size, const char *dir, const char *filenames[]) {
     if (dir == NULL || filenames == NULL || dst == NULL) return;
     size_t len = strlen(dir);
-    for(int i = 0; i < sizeof(filenames); i++) {
+    for(int i = 0; filenames[i] != NULL; i++) {
         char *filename = filenames[i];
-        printf("filename: %s\n", filename);
-        // if (len > 0 && dir[len-1] == '/')
-        //     snprintf(dst, size, "%s%s", dir, filename);
-        // else
-        //     snprintf(dst, size, "%s/%s", dir, filename);
+        if (len > 0 && dir[len-1] == '/')
+            snprintf(dst, size, "%s%s", dir, filename);
+        else
+            snprintf(dst, size, "%s/%s", dir, filename);
     }
 }
 
