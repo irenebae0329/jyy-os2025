a0a36afdd835ae57a801410bea753ef57bfcd97b
tracer-nju <tracer@nju.edu.cn>
2025-05-09 16:50:02 +0800

    Darwin W22WY4FD6V 24.3.0 Darwin Kernel Version 24.3.0: Thu Jan  2 20:24:16 PST 2025; root:xnu-11215.81.4~3/RELEASE_ARM64_T6000 arm64 16:50  up 16 days, 19:12, 1 user, load averages: 3.70 3.31 3.70

--- .shadow/labyrinth/labyrinth.c
+++ .shadow/labyrinth/labyrinth.c
@@ -32,7 +32,7 @@ int main(int argc, char *argv[]) {
     };
 
     joinPath(mapfilePath, sizeof(mapfilePath), PROJECT_ROOT, (const char *[]){"maps", "map1.txt"});
-    printf("mapfilePath: %s\n", mapfilePath);
+    // printf("mapfilePath: %s\n", mapfilePath);
 
     
     // while ((opt = getopt_long(argc, argv, "m:p:", long_options, &option_index)) != -1) {
