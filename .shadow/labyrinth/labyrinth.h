b9c6be3183a7f9b23d898e58d11b94be9b6b177d
tracer-nju <tracer@nju.edu.cn>
2025-05-09 15:35:09 +0800

    Darwin W22WY4FD6V 24.3.0 Darwin Kernel Version 24.3.0: Thu Jan  2 20:24:16 PST 2025; root:xnu-11215.81.4~3/RELEASE_ARM64_T6000 arm64 15:35  up 16 days, 17:57, 1 user, load averages: 4.53 4.11 4.10

--- .shadow/labyrinth/labyrinth.h
+++ .shadow/labyrinth/labyrinth.h
@@ -17,7 +17,7 @@ typedef struct {
     int col;
 } Position;
 
-
+void printUsage();
 bool isValidPlayer(char playerId);
 bool loadMap(Labyrinth *labyrinth, const char *filename);
 Position findPlayer(Labyrinth *labyrinth, char playerId);
